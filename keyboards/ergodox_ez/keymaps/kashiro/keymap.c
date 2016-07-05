97d3a74711bcbd46605c25a2dd99b0171e9def24
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2016-07-05 10:32:24 -0400

    formatted

--- keyboards/ergodox_ez/keymaps/kashiro/keymap.c
+++ keyboards/ergodox_ez/keymaps/kashiro/keymap.c
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                KC_SPC,GUI_T(KC_NO),KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_BSPC,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LEFT,KC_RGHT,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_ESC, KC_ENT
+        KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+        TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                     KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_BSPC,
+        MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                             KC_UP,  KC_DOWN,KC_LEFT,KC_RGHT,          KC_FN1,
+        KC_LALT,        CTL_T(KC_ESC),
+        KC_PGUP,
+        KC_PGDN,KC_ESC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
